3d2162416cd5d75424f26fa6a5120bca6bc8b1bb
Ryland <mrl56413@163.com>
2026-09-14 20:19:57 +0800

    update fault

--- fault/fault_record.c
+++ fault/fault_record.c
@@ -4,17 +4,22 @@
 
 void print_fault(FaultRecord *record)
 {
-    // FaultRecord *save_rec;
-    // save_rec = (FaultRecord *)malloc(sizeof(FaultRecord));
-    // save_rec->address = record->address;
-    // save_rec->id = record->id;
-    // save_rec->pc = record->pc;
-    // save_rec->reason = record->reason;
-    // save_rec->type = record->type;
-    printf("address:0x%x\n", record->address);
-    printf("id:%x\n", record->id);
-    printf("pc:%x\n", record->pc);
-    printf("reason:%d\n", record->reason);
-    printf("Exception_type:%d\n", record->type);
-    // free(save_rec);
+       // FaultRecord *save_rec;
+       // save_rec = (FaultRecord *)malloc(sizeof(FaultRecord));
+       // save_rec->address = record->address;
+       // save_rec->id = record->id;
+       // save_rec->pc = record->pc;
+       // save_rec->reason = record->reason;
+       // save_rec->type = record->type;
+       printf("address:0x%x\n",
+              record->address);
+       printf("id:%x\n",
+              record->id);
+       printf("pc:%x\n",
+              record->pc);
+       printf("reason:%d\n",
+              record->reason);
+       printf("Exception_type:%d\n",
+              record->type);
+       // free(save_rec);
 }
